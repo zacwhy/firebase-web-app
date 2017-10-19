b3b5e56faa6680b9db6097969edc19a20e8a4276
ZY <zacwhy@gmail.com>
2017-10-19 10:26:20 +0800

    Change index page

--- public/scripts/library.js
+++
@@ -1,7 +0,0 @@
-function orderBy(selector, ascending = true) {
-  return (a, b) => selector(ascending ? a : b).localeCompare(selector(ascending ? b : a))
-}
-
-function orderByDesc(selector) {
-  return orderBy(selector, false)
-}
